04b7dbb41b43fa85df647c0a14a018d9967a96cc
Ryan S. Northrup <rnorthrup@newleaders.com>
2015-07-07 17:44:03 -0700

    Two very rudimentary test cases for Bales apps.

---
+++ test/bin/simple-app.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+$LOAD_PATH.unshift(File.expand_path(File.join(
+                                     File.dirname(__FILE__),
+                                     '../../lib'
+                                   )))
+require 'bales'
+
+module SimpleApp
+  class Application < Bales::Application
+    version "0.0.1"
+  end
+
+  class Command < Bales::Command
+    action do
+      Bales::Command::Help.run
+    end
+
+    class Smack < Command
+      option :weapon,
+             type: String,
+             description: "Thing to smack with",
+             short_form: '-w',
+             long_form: '--with'
+
+      action do |victims, opts|
+        suffix = opts[:weapon] ? " with a #{opts[:weapon]}" : ""
+
+        if victims.none?
+          puts "You have been smacked#{suffix}."
+        else
+          victims.each do |victim|
+            puts "#{victim} has been smacked#{suffix}."
+          end
+        end
+      end
+    end
+  end
+end
+
+SimpleApp::Application.parse_and_run
